ab5573025264e9311412762ff981e8f857b7d996
Chandan Singh <chandan_singh@berkeley.edu>
2020-07-21 20:45:59 -0700

    major train refactor

--- train.py
+++ train.py
@@ -21,6 +21,8 @@ from sklearn.svm import SVC
 from sklearn import metrics
 from sklearn.feature_selection import SelectFromModel
 import eli5
+import os.path
+from imodels import RuleListClassifier, RuleFit, SLIM, GreedyRuleList
 from imblearn.over_sampling import RandomOverSampler, SMOTE
 from sklearn.model_selection import KFold
 import pandas as pd
@@ -66,6 +68,148 @@ def balance(X, y, balancing='ros', balancing_ratio: float=1):
     X_r, Y_r = sampler.fit_resample(X, y)   
     return X_r, Y_r
 
+def get_model(model_type):
+    if model_type == 'rf':
+        m = RandomForestClassifier(n_estimators=100)
+    elif model_type == 'dt':
+        m = DecisionTreeClassifier()
+    elif model_type == 'logistic':
+        m = LogisticRegression(solver='lbfgs')
+    elif model_type == 'svm':
+        m = SVC(gamma='scale')
+    elif model_type == 'mlp2':
+        m = MLPClassifier()
+    elif model_type == 'gb':
+        m = GradientBoostingClassifier()
+    return m
+
+    
+def select_features(feature_selection, feature_selection_num, X_train, X_test1, X_test2, y_train):
+    '''Select features and return the selected ones
+    '''
+    # don't perform any features selection
+    if feature_selection is None:
+        return X_train, X_test1, X_test2, np.ones(len(feat_names)).astype(np.bool)
+
+
+    # perform some feature selection
+    if 'stab' in feature_selection:
+        if feature_selection == 'select_stab_lasso':
+            feature_selector_model = LogisticRegression(penalty='l1', solver='liblinear')
+        feature_selector = StabilitySelection(base_estimator=feature_selector_model,
+                                              lambda_name='C',
+                                              lambda_grid=np.logspace(-5, -1, 20),
+                                              max_features=feature_selection_num)
+    else:
+        if feature_selection == 'select_lasso':
+            feature_selector_model = Lasso()
+        elif feature_selection == 'select_rf':
+            feature_selector_model = RandomForestClassifier()
+        feature_selector = SelectFromModel(feature_selector_model, threshold=-np.inf,
+                                           max_features=feature_selection_num)
+    feature_selector.fit(X_train, y_train)
+    X_train = feature_selector.transform(X_train)
+    X_test1 = feature_selector.transform(X_test1)
+    X_test2 = feature_selector.transform(X_test2)
+    support = np.array(feature_selector.get_support())
+
+    return X_train, X_test1, X_test2, support
+
+def predict_over_folds(cv_folds, X, y, X_test1, X_test2,
+                       m, sample_weights, balancing, train_idxs, model_type):
+    '''loop over folds
+    Returns
+    -------
+    predictions
+        predictions on test should be based on the model which has best performance on individual fold
+    fitted models
+    importances
+    '''
+
+    models = []
+    imps = []
+    preds_list = []
+    preds_test1_list = []    
+    preds_test2_list = []    
+    ys = []
+    splits = KFold(n_splits=len(train_idxs)).split(train_idxs)
+    for cv_idx, cv_val_idx in splits:
+
+        # get sample indices
+        idxs_cv = cv_folds.isin(cv_idx + 1)
+        idxs_val_cv = cv_folds.isin(cv_val_idx + 1)
+        X_train_cv, Y_train_cv = X[idxs_cv], y[idxs_cv]
+        X_val_cv, Y_val_cv = X[idxs_val_cv], y[idxs_val_cv]
+
+        # fit with appropriate weighting
+        if balancing == 'sample_weights':
+            m.fit(X_train_cv, Y_train_cv, sample_weight=sample_weights[idxs_cv])
+        else:
+            X_train_r_cv, Y_train_r_cv = balance(X_train_cv, Y_train_cv, balancing, balancing_ratio)
+            m.fit(X_train_r_cv, Y_train_r_cv)
+        
+        # append lists
+        preds_list.append(m.predict_proba(X_val_cv))
+        preds_test1_list.append(m.predict_proba(X_test1))
+        preds_test2_list.append(m.predict_proba(X_test2))                    
+        models.append(deepcopy(m))
+        imps.append(get_feature_importance(m, model_type, X_val_cv, Y_val_cv))
+        ys.append(Y_val_cv)
+        
+    return models, imps, preds_list, preds_test1_list, preds_test2_list, ys
+
+def specificity_score(y_true, y_pred):
+    tn, fp, fn, tp = metrics.confusion_matrix(y_true, y_pred).ravel()
+    return tn / (tn + tp)
+
+scorers = {
+   'balanced_accuracy': metrics.balanced_accuracy_score, 
+   'accuracy': metrics.accuracy_score,
+   'precision': metrics.precision_score, 
+   'sensitivity': metrics.recall_score, 
+   'specificity': specificity_score,
+   'f1': metrics.f1_score, 
+   'roc_auc': metrics.roc_auc_score,
+   'precision_recall_curve': metrics.precision_recall_curve, 
+   'roc_curve': metrics.roc_curve,
+   'tn': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[0],
+   'fp': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[1],
+   'fn': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[2],
+   'tp': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[3],
+}
+
+def append_score_results(scores, pred, y, prefix=''):
+    '''Score given one pred, y
+    '''
+    
+    for k in scorers.keys():
+        k_new = prefix + '_' + k
+        if not k_new in scores.keys():
+            scores[k_new] = []
+        
+        print(k_new)
+        if 'roc' in k or 'curve' in k:
+            scores[k_new].append(scorers[k](y, pred))
+        else:
+            pred_thresh = (pred > 0.5).astype(int)
+            print(np.unique(y), np.unique(pred_thresh))
+#             scores[k_new].append(scorers[k](y, pred_thresh))
+        
+        
+
+def get_scores(predictions_list, predictions_test1_list, predictions_test2_list,
+               Y_train, Y_test1, Y_test2):
+    
+    # score cv folds
+    scores = {}
+    for preds_list, Y_list in zip([predictions_list, predictions_test1_list, predictions_test2_list], 
+                                  [Y_train, Y_test1, Y_test2]):
+        for i in range(len(preds_list)):
+            append_score_results(scores, preds_list[i], Y_list[i], prefix='cv')
+        
+        append_score_results(scores, np.vstack(preds_list), np.vstack(Y_list))
+    
+    return scores
 
 
 def train(df: pd.DataFrame, feat_names: list, model_type='rf', outcome_def='iai_intervention',
@@ -90,129 +234,36 @@ def train(df: pd.DataFrame, feat_names: list, model_type='rf', outcome_def='iai_
     X_test2, Y_test2 = X[idxs_test2], y[idxs_test2]
     
     # get model
-    if model_type == 'rf':
-        m = RandomForestClassifier(n_estimators=100)
-    elif model_type == 'dt':
-        m = DecisionTreeClassifier()
-    elif model_type == 'logistic':
-        m = LogisticRegression(solver='lbfgs')
-    elif model_type == 'svm':
-        m = SVC(gamma='scale')
-    elif model_type == 'mlp2':
-        m = MLPClassifier()
-    elif model_type == 'gb':
-        m = GradientBoostingClassifier()
+    m = get_model(model_type)
     
     # feature selection
-    feature_selector = None
-    if feature_selection is not None:
-        if 'stab' in feature_selection:
-            if feature_selection == 'select_stab_lasso':
-                feature_selector_model = LogisticRegression(penalty='l1', solver='liblinear')
-            feature_selector = StabilitySelection(base_estimator=feature_selector_model, lambda_name='C',
-                                                  lambda_grid=np.logspace(-5, -1, 20),
-                                                  max_features=feature_selection_num)
-        else:
-            if feature_selection == 'select_lasso':
-                feature_selector_model = Lasso()
-            elif feature_selection == 'select_rf':
-                feature_selector_model = RandomForestClassifier()
-            feature_selector = SelectFromModel(feature_selector_model, threshold=-np.inf,
-                                               max_features=feature_selection_num)
-            
-            
-        
-        feature_selector.fit(X_train, y_train)
-        X = feature_selector.transform(X)
-        X_test1 = feature_selector.transform(X_test1)
-        X_test2 = feature_selector.transform(X_test2)
-        support = np.array(feature_selector.get_support())
-    else:
-        support = np.ones(len(feat_names)).astype(np.bool)    
+    X_train, X_test1, X_test2, support = \
+        select_features(feature_selection, feature_selection_num, X_train, X_test1, X_test2, y_train)
     
-
-    def specificity_score(y_true, y_pred):
-        tn, fp, fn, tp = metrics.confusion_matrix(y_true, y_pred).ravel()
-        return tn / (tn + tp)
     
-    scorers = {
-       'balanced_accuracy': metrics.balanced_accuracy_score, 
-       'accuracy': metrics.accuracy_score,
-       'precision': metrics.precision_score, 
-       'sensitivity': metrics.recall_score, 
-       'specificity': specificity_score,
-       'f1': metrics.f1_score, 
-       'roc_auc': metrics.roc_auc_score,
-       'precision_recall_curve': metrics.precision_recall_curve, 
-       'roc_curve': metrics.roc_curve,
-       'tn': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[0],
-       'fp': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[1],
-       'fn': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[2],
-       'tp': lambda y_true, y_pred: metrics.confusion_matrix(y_true, y_pred).ravel()[3],
-    }
-    scores_cv = {s: [] for s in scorers.keys()}
-    scores_test1 = {s: [] for s in scorers.keys()}
-    scores_test2 = {s: [] for s in scorers.keys()}
-    imps = {'model': [], 'imps': []}
-
+    # prediction
+    models, imps, predictions_list, predictions_test1_list, predictions_test2_list, y_train = \
+        predict_over_folds(df.cv_fold[idxs_train], X_train, y_train,
+                           X_test1, X_test2, m, sample_weights[idxs_train],
+                           balancing, train_idxs, model_type)
     
-    cv_folds_train = train_idxs
-    kf = KFold(n_splits=len(cv_folds_train))
-    for cv_idx, cv_val_idx in kf.split(cv_folds_train):
-        
-        # get sample indices
-        idxs_cv = df.cv_fold.isin(cv_idx + 1)
-        idxs_val_cv = df.cv_fold.isin(cv_val_idx + 1)
-        X_train_cv, Y_train_cv = X[idxs_cv], y[idxs_cv]
-        X_val_cv, Y_val_cv = X[idxs_val_cv], y[idxs_val_cv]
-
-        # fit with appropriate weighting
-        if balancing == 'sample_weights':
-            m.fit(X_train_cv, Y_train_cv, sample_weight=sample_weights[idxs_cv])
-        else:
-            X_train_r_cv, Y_train_r_cv = balance(X_train_cv, Y_train_cv, balancing, balancing_ratio)
-            m.fit(X_train_r_cv, Y_train_r_cv)
-
-        # get preds
-        preds = m.predict(X_val_cv)
-        preds_test1 = m.predict(X_test1)
-        preds_test2 = m.predict(X_test2)
-        if model_type == 'svm':
-            preds_proba = preds
-            preds_test1_proba = preds_test1
-            preds_test2_proba = preds_test2
-        else:
-            preds_proba = m.predict_proba(X_val_cv)[:, 1]
-            preds_test1_proba = m.predict_proba(X_test1)[:, 1]
-            preds_test2_proba = m.predict_proba(X_test2)[:, 1]
-
-
-        # add scores
-        for s in scorers.keys():
-            scorer = scorers[s]
-            if 'roc' in s or 'curve' in s:
-                # print(np.unique(preds, return_counts=True), np.unique(Y_val_cv, return_counts=True))
-                scores_cv[s].append(scorer(Y_val_cv, preds_proba))
-                scores_test1[s].append(scorer(Y_test1, preds_test1_proba))
-                scores_test2[s].append(scorer(Y_test2, preds_test2_proba))
-            else:
-                scores_cv[s].append(scorer(Y_val_cv, preds))
-                scores_test1[s].append(scorer(Y_test1, preds_test1))
-                scores_test2[s].append(scorer(Y_test2, preds_test2))
-        imps['model'].append(deepcopy(m))
-        imps['imps'].append(get_feature_importance(m, model_type, X_val_cv, Y_val_cv))
-
+    # scoring
+    scores = get_scores(predictions_list, predictions_test1_list, predictions_test2_list, y_train, Y_test1, Y_test2)
+    
+    
     # save results
-    # os.makedirs(out_dir, exist_ok=True)
-    results = {'metrics': list(scorers.keys()), 
-               'cv': scores_cv, 
-               'test1': scores_test1,
-               'test2': scores_test2,
-               'imps': imps,
-#                'feat_names': feat_names,
-               'model_type': model_type,
-               'balancing': balancing,
-               'feat_names_selected': np.array(feat_names)[support],               
-               'balacing_ratio': balancing_ratio,
-              }
+    os.makedirs(os.path.dirname(out_name), exist_ok=True)
+    results = {
+        'models': models,
+        'imps': imps,        
+        **scores,
+        
+        'metrics': list(scorers.keys()), 
+        
+        # params
+        'model_type': model_type,
+        'balancing': balancing,
+        'feat_names_selected': np.array(feat_names)[support],               
+        'balacing_ratio': balancing_ratio,
+    }
     pkl.dump(results, open(out_name, 'wb'))
